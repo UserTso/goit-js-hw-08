0971cdfd1293a88b718b0e0587796a592cc6ef01
User Tso <tso20101983@gmail.com>
2022-08-24 15:51:30 +0300

    change

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -10,10 +10,27 @@ const player = new Player(iframe);
 const KEY_STORAGE = "videoplayer-current-time";
 
 player.on('timeupdate', throttle(event => {
-    localStorage.setItem(KEY_STORAGE, JSON.stringify(event.seconds))
+    localStorage.setItem(KEY_STORAGE, JSON.stringify(event.seconds));
     }, 1000) );
 const saveTime = localStorage.getItem(KEY_STORAGE);
 
 player.setCurrentTime(saveTime);
    
 
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+
+// const iframe = document.querySelector('#vimeo-player');
+// const player = new Player(iframe);
+
+// const KEY_STORAGE = 'videoplayer-current-time';
+
+// player.on(
+//   'timeupdate',
+//   throttle(event => {
+//     localStorage.setItem(KEY_STORAGE, JSON.stringify(event.seconds));
+//   }, 1000)
+// );
+// const getTimeMemory = localStorage.getItem(KEY_STORAGE);
+
+// player.setCurrentTime(getTimeMemory);
